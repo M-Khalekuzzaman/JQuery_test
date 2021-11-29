c6da61e38c3971df4063813cb64a624fb30d15df
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-29 09:52:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// $("#passlogin").click(function(){
+
+//  var password1 = $("#pass1").val();
+//  var password2 = $("#pass2").val();
+
+
+
+//  if(password1!= "" && password2!=""){
+
+//    if(password1==password2){
+//        alert("Successfully login")
+//    }else{
+//         alert("Try again")
+//    }
+
+
+
+//  }else{
+//      alert("please enter password")
+//  }
+
+
+// })
+
+$("#btn").click(function(){
+
+
+$("#para").slideUp(2000).slideDown(2000).slideToggle(2000);
+
+
+
+})
